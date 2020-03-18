1e7b1fe3b98e2c93a2287ad9a1b3973c2932936c
antoinetardivel <tardivelantoine@gmail.com>
2020-03-18 16:38:10 +0100

    commentaires

---
+++ Article/action-create.php
@@ -0,0 +1,132 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+
+	<?php 
+
+	function ctrlSaisies($saisie) {
+
+	  // Suppression des espaces (ou d'autres caractères) en début et fin de chaîne
+	  $saisie = trim($saisie);
+	  // Suppression des antislashs d'une chaîne
+	  $saisie = stripslashes($saisie);
+	  // Conversion des caractères spéciaux en entités HTML 
+	  $saisie = htmlentities($saisie);
+
+	  return $saisie;
+	}
+
+	$hostBD = "localhost";
+	$nomBD = "blogart20";
+	$userBD = 'root';
+	$passBD = '';
+	try {
+	        $connection = new PDO("mysql:dbname=$nomBD;host=$hostBD;charset=utf8", $userBD, $passBD);
+	        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	    } 
+	    catch (PDOException $error) {
+	        die('Failed to connect : ' . $error->getMessage());
+	    }
+
+$NumArt = " ";
+$DtCreA = " ";
+$LibTitrA = $_POST['LibTitrA'];
+$LibChapoA = $_POST['LibChapoA'];
+$LibAccrochA = $_POST['LibAccrochA'];
+$Parag1A = $_POST['Parag1A'];
+$LibSsTitr1 = $_POST['LibSsTitr1'];
+$Parag2A = $_POST['Parag2A'];
+$LibSsTitr2 = $_POST['LibSsTitr2'];
+$Parag3A = $_POST['Parag3A'];
+$LibConclA = $_POST['LibConclA'];
+$UrlPhotA = $_POST['UrlPhotA'];
+$Likes = 0;
+$NumAngl = " ";
+$NumThem = " ";
+$NumLang = $_POST['NumLang'];
+
+$PseudoAuteur = $_POST['PseudoAuteur'];
+$EmailAuteur = $_POST['EmailAuteur'];
+$TitrCom = $_POST['TitrCom'];
+$LibCom = $_POST['LibCom'];
+$NumArt = $_POST['quelart'];
+echo $quelart;
+echo "Informations reçus : ".$PseudoAuteur." ".$EmailAuteur." ".$TitrCom." ".$LibCom;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "<br/>Passage de condition 1 : ".$PseudoAuteur." ".$EmailAuteur." ".$TitrCom." ".$LibCom;
+
+    $Submit = isset($_POST['Submit']) ? $_POST['Submit'] : '';
+
+    echo "<br>"."Passage de condition2 : ".$PseudoAuteur." ".$EmailAuteur." ".$TitrCom." ".$LibCom;
+
+    $erreur = false;
+    $NumLang = 0;
+
+    $PseudoAuteur = (ctrlSaisies($_POST["PseudoAuteur"]));
+    $EmailAuteur = (ctrlSaisies($_POST["EmailAuteur"]));
+    $TitrCom = (ctrlSaisies($_POST["TitrCom"]));
+    $LibCom = (ctrlSaisies($_POST["LibCom"]));
+
+    $requete = "SELECT MAX(NumCom) AS NumCom FROM COMMENT";
+
+    $result = $connection->query($requete);
+    
+    $tuple = $result->fetch();
+    $tuple = (int)end($tuple);
+    $tuple++;
+    $NumCom = $tuple;
+    $DtCreC = date("Y-m-d");
+    $DtCreCd = date("H-i-s");
+    $DtCreC = $DtCreC." ".$DtCreCd;
+
+    try {
+      $connection->beginTransaction();
+
+      echo "<br>"."Dernier echo avant le SEND : ".$NumCom." ".$DtCreC." ".$PseudoAuteur." ".$EmailAuteur." ".$TitrCom." ".$LibCom." ".$NumArt."<br><br>";
+      $query = $connection->prepare("INSERT INTO COMMENT (NumCom, DtCreC, PseudoAuteur, EmailAuteur, TitrCom, LibCom, NumArt) VALUES (:NumCom, :DtCreC, :PseudoAuteur, :EmailAuteur, :TitrCom, :LibCom, :NumArt)");
+
+      $query->execute(
+        array(
+          ':NumCom' => $NumCom,
+          ':DtCreC' => $DtCreC, 
+          ':PseudoAuteur' => $PseudoAuteur,
+          ':EmailAuteur' => $EmailAuteur,
+          ':TitrCom' => $TitrCom,
+          ':LibCom' => $LibCom,
+          ':NumArt' => $NumArt
+        )
+      );
+
+      $langid = $NumCom;
+
+      echo $NumCom, $DtCreC, $PseudoAuteur, $EmailAuteur, $TitrCom, $LibCom, $NumArt ;
+
+      $connection->commit();
+
+      $query->closeCursor();
+
+      header("Location:index.php");
+    }
+    catch (PDOException $e) {
+      die('Failed to insert Commentaire : ' . $e->getMessage());
+      $connection->rollBack();
+    }
+  
+
+}
+
+	
+
+?>
+
+
+
+
+</body>
+</html>
---
+++ Article/create.php
@@ -0,0 +1,83 @@
+<?php require 'header.php'; ?>
+<div class="container">
+  <div class="card mt-5">
+    <div class="card-header">
+    <h2>Ajoutez un article</h2>
+    </div>
+    <div class="card-body">
+      <?php if(!empty($message)): ?> <!-- Si $message n'est pas vide -->
+        <div class="alert alert-success"> <!-- Alerte succes -->
+          <?= $message; ?> <!-- Echo de $message -->
+        </div>
+      <?php endif; ?>
+      <form class="container-contact-us" action="action-create.php" method="POST"> <!-- Si le formulaire est activé, on redirige vers action-create.php -->
+        <div class="form-group">
+            <span class="span-text">Titre article</span> 
+            <input class="form-control" type="text" name="LibTitrA" maxlength="100" required> <!-- Définition du taille max et impose à se que le champ soit complété -->
+						<span 
+            class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Url illustration</span> 
+            <input class="form-control" type="text" name="UrlPhotA" maxlength="62" required> <!-- Définition du taille max et impose à se que le champ soit complété -->
+						<span 
+            class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Libellé chapeau</span> 
+            <textarea class="form-control" type="text" name="LibChapoA" rows="3" maxlength="500" required></textarea>
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Libellé Accroche</span>
+            <textarea class="form-control" type="text" name="LibAccrochA" rows="3" maxlength="100" required></textarea>
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Libellé paragraphe 1</span>
+            <textarea class="form-control" type="text" name="Parag1A" rows="3" maxlength="1200" required></textarea>
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Libellé sous-titre 1</span> 
+            <input class="form-control" type="text" name="LibSsTitr1" maxlength="100" required> <!-- Définition du taille max et impose à se que le champ soit complété -->
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Libellé paragraphe 2</span>
+            <textarea class="form-control" type="text" name="Parag2A" rows="3" maxlength="1200" required></textarea>
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Libellé sous-titre 2</span> 
+            <input class="form-control" type="text" name="LibSsTitr2" maxlength="100" required> <!-- Définition du taille max et impose à se que le champ soit complété -->
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Libellé paragraphe 3</span>
+            <textarea class="form-control" type="text" name="Parag3A" rows="3" maxlength="1200" required></textarea>
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Libellé conclusion</span>
+            <textarea class="form-control" type="text" name="LibConclA" rows="3" maxlength="800" required></textarea>
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Quel angle ?</span>
+						<input class="form-control" type="text" name="NumLang" maxlength="4" required>
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+            <span class="span-text">Mot clé</span>
+						<input class="form-control" type="text" name="NumLang" maxlength="4" required>
+						<span class="line"></span>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-info">Ajouter l'article</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<?php require 'footer.php'; ?> <!-- Va chercher le fichier footer.php -->
