66f703b38767da437e4e68542e1b179fcf18caae
Bright2196 <claireriussetcr@gmail.com>
2020-03-24 18:17:26 +0100

    insert-homepage2

--- FRONT/Home page/index.php
+++ FRONT/Home page/index.php
@@ -62,11 +62,11 @@
         <iframe class="playlist" src="https://play.soundsgood.co/embed/5e754bed7381827590892c68?color=323733&hideCurator=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; encrypted-media" allowtransparency="true"></iframe>
         
          <div class="reseaux-sociaux">
-            <img class ="reseaux_sociauxt" src="../images/twitter.png" alt="twitter_logo">
+            <img class ="reseaux_sociauxt" src="../images/twitter1.png" alt="twitter_logo">
         
-            <img class="reseaux_sociauxi" src="../images/insta.png" alt="instagram_logo">
+            <img class="reseaux_sociauxi" src="../images/insta1.png" alt="instagram_logo">
         
-            <img class="reseaux_sociauxm" src="../images/medium.png" alt="medium_logo">
+            <img class="reseaux_sociauxm" src="../images/medium1.png" alt="medium_logo">
         </div>
        
 </aside>
