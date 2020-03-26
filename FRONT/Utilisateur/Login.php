f4bcdcda8a303556eaf48d5d7ba0c23c9aebfaf2
LeoPhilip33 <leo.philip@mmibordeaux.com>
2020-03-26 18:27:43 +0100

    login

--- FRONT/Utilisateur/Login.php
+++ FRONT/Utilisateur/Login.php
@@ -7,57 +7,33 @@
         <title>Horry Bord - Connexion</title>
     </head>
     <body>
-        <img src="../images/Illustration_header.jpg" alt="Image header">
-
-        <div class = "inscription">
-            <div class ="gauche_inscription">
-                <h1> Connexion </h1>
-                <p> Si la terreur ne te fais pas peur ... Connecte-toi ! </p>
-                <div class="googlefacebook">
-                    <div> <img class="img-connexion" src="../images/fb.png" alt="Image de facebook"> </div>
-                    <div class="centerreseau" > Se conncter avec Facebook </div>
-                </div>
-
-                <div class="googlefacebook">
-                    <div> <img class="img-connexion" src="../images/google.png" alt="Image de google"> </div>
-                    <div class="centerreseau" > Se connecter avec Google </div>
-                </div>
-
-                OU
-
-                <form method="POST" >
-                    
-                    <div class="gaucheconnexion">
-                        <div><label> Identifiant</label></div>
-                        <div><input type="text" name="Login" maxlength="30" required></div>
-                        
-
-                        <div><label> Mot de passe</label></div>
-                        <div><input type="Password" name="Pass" maxlength="15" required></div>
-                        
-                        <div id="mdpoublie">
-                            Mot de passe oublié ?
-                        </div>
-
-                        <button type ="submit" name="confirm_login"> Se connecter </button>
-
-                        <div> 
-                            <a href="#" > Vous n'avez pas de compte ? Inscription </a>
-                        </div>
-                    <div>
+        <?php require '../Require_php/Illustration_header.php'; ?><!--header-->
+        <?php require '../Require_php/barnav.php'; ?><!--navbar-->
+        <!-- mennu -->
+        <div class="content">
+            <div class="fichecontact">
+                <h3 class="titrecont">Contact</h3>
+                <form class="formcont" method="POST">
+                    <p class="phrasecont">Si la terreur ne te fais pas peur… Connecte-nous!</p>
+                    <div class="form-group">
+                        <label for="email">Identifiant :</label>
+                        <input type="email" name="email" class="form-control" placeholder="Adresse Email" maxlength="45" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="objet">Mot de passe :</label>
+                        <input type="text" name="objet" class="form-control" placeholder="Objet" maxlength="45" required>
+                    </div>
+                    <div class="form-group">
+                        <button type="envoyer" value="ok"class="btn btn-primary">Envoyer</button>
+                    </div>
                 </form>
-                <?php if(isset($error)){
-                    echo $error;
-                }
+                <?php 
+                    if(isset($error)){
+                        echo $error;
+                    }
                 ?>
-
-                <div class="droit_inscription">
-                    <img class="connexionIMGhorror" src="../images/connexion.png" alt="Une image de zombie">
-                </div>
-
             </div>
         </div>
-
     </body>
 </html>
 
