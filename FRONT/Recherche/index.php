7dc7b4c8844d7551c93566c2f746d1ffc206c0f8
antoinetardivel <tardivelantoine@gmail.com>
2020-03-26 23:14:46 +0100

    barre recherche finie

--- FRONT/Recherche/index.php
+++ FRONT/Recherche/index.php
@@ -21,9 +21,9 @@
                     $articles = $connection->query('SELECT LibTitrA FROM article ORDER BY NumArt DESC');
                     if(isset($_GET['search']) AND !empty($_GET['search'])) {
                         $search = htmlspecialchars($_GET['search']);
-                        $articles = $connection->query('SELECT LibTitrA FROM article WHERE LibTitrA LIKE "%'.$search.'%" ORDER BY NumArt DESC');
+                        $articles = $connection->query('SELECT NumArt FROM article WHERE LibTitrA LIKE "%'.$search.'%" ORDER BY NumArt DESC');
                         if($articles->rowCount() == 0) {
-                            $articles = $connection->query('SELECT LibTitrA FROM article WHERE CONCAT(LibTitrA, LibChapoA) LIKE "%'.$search.'%" ORDER BY NumArt DESC');
+                            $articles = $connection->query('SELECT NumArt FROM article WHERE LibChapoA LIKE "%'.$search.'%" ORDER BY NumArt DESC');
                         }
 
                     }
@@ -31,17 +31,17 @@
                 <?php if($articles->rowCount() > 0) { ?>
                 <?php
                 while($rows = $articles->fetch()) {
-                    $titre=$rows['LibTitrA'];
-                    $ContArt = "SELECT * FROM article WHERE LibTitrA='$titre'";
+                    $titre=$rows['NumArt'];
+                    $ContArt = "SELECT * FROM article WHERE NumArt='$titre'";
                     $result = $connection->query($ContArt);
                     if ($result) {
                         while ($tuple = $result->fetch()) {
                 ?>
                         <div class="traitsepare"></div>
-                        <p class="titreartrech"><?= "<a style='text-decoration:none; color:#F8F8F8;' href=''>".$titre."</a>" ?></p>
+                        <p class="titreartrech"><?= "<a style='text-decoration:none; color:#F8F8F8;' href=''>".$tuple["LibTitrA"]."</a>" ?></p>
                         <p class="datemotcle"><?=$tuple["DtCreA"]?></p>
                         <div class="rowrech">
-                            <div class="imgrech"></div>
+                            <div class="imgrech" style="background-image:url(<?=$tuple["UrlPhotA"]?>);"></div>
                             <div class="divconttext">
                                 <p class="accrocherech"><?=$tuple["LibAccrochA"]?></p>
                                 <a style="text-decoration:none" href="../ArticleType/index.php?id=<?=$tuple["NumArt"]?>"><div class="btnvp" id="btnvp"><p>Voir plus</p></div></a>
