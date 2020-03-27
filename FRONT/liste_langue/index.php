ad60444f9452c17036ece558e31042a0afa95094
Bright2196 <claireriussetcr@gmail.com>
2020-03-27 19:46:17 +0100

    edit langue

--- FRONT/liste_langue/index.php
+++ FRONT/liste_langue/index.php
@@ -9,37 +9,38 @@
 <body>
     <?php require '../Require_php/barnav_dashbord.php'; ?><!--navbar-->
     <div class="container">
+    
         <div class="tableau_global">
         <h3 class="creat_php">Liste des langues</h3>
         <p class="texte1">Répertoire de toutes les langues</p>
   
             <table class="tableau_langues">
-                    <tr>
+                    <tr class="en-tete">
                         <th>Libellés courts</th>
                         <th>Libellés longs</th>
                         <th>Numéros pays</th>
                         <th>Actions</th>
                     </tr>
                 
-                    <tr>
+                    <tr class="ligne_1">
                         <td>Allemand(e)</td>
                         <td>Langue allemande</td>
                         <td>ALLE</td>
                         <td><button class="edit">éditer</button> <button class="delete">supprimer</button></td>
                     </tr>
-                    <tr>
+                    <tr class="ligne_2">
                         <td>Anglais(e)</td>
                         <td>Langue anglaise</td>
                         <td>ANGL</td>
                         <td><button class="edit">éditer</button> <button class="delete">supprimer</button></td>
                     </tr>
-                    <tr>
+                    <tr class="ligne_3">
                         <td>Bulgare</td>
                         <td>Langue Bulgare</td>
                         <td>BULG</td>
                         <td><button class="edit">éditer</button> <button class="delete">supprimer</button></td>
                     </tr>
-                    <tr>
+                    <tr class="ligne_4">
                         <td>Espagnol(e)</td>
                         <td>Langue espagnole</td>
                         <td>ESPA</td>
@@ -47,6 +48,7 @@
                     </tr>
              </table>
             </div>
+            
     </div>
 </body>
 </html>
